a6e5805d347b18ee661c71ef8cb2d91847e2c919
Sébastien Barbier <contact@sebastienbarbier.com>
2021-09-14 17:57:30 +0200

    First version with run.sh to register and load configuration files from server
    Related to #1 and #2

---
+++ run.sh
@@ -0,0 +1,37 @@
+# Install python dependancies
+pip3 install -r requirements.txt
+
+# If ./.env file exist, we export variables to current system to display later
+if [ -f .env ]; then
+  export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
+fi
+
+# Set default port to access monitor-client web interface
+if [[ -z "${PORT}" ]]; then export PORT=8001
+fi
+# Set protocol to load nginx
+if [[ -z "${PROTOCOL}" ]]; then export PROTOCOL=http
+fi
+# Set default port to access dashboard
+if [[ -z "${SERVER}" ]]; then export SERVER=localhost:8000
+fi
+
+echo "Register"
+python3 scripts/register.py
+
+# IF register.py fail
+if [ $? -ne 0 ]; then
+	exit
+fi
+
+echo "Load-config"
+python3 scripts/load-config.py
+
+# IF load-config.py return code 0
+if [ $? -ne 0 ]; then
+	exit
+fi
+
+docker-compose up -d
+
+echo "Access prometheus at localhost:$PORT"
