334d543fff4ce714725406bf1bb3417da5886b38
Sébastien Barbier <contact@sebastienbarbier.com>
2023-03-22 22:34:15 +0100

    Add FastAPI to heartbeats to handle request from backend

--- heartbeats/docker-entrypoint.sh
+++ heartbeats/docker-entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 pip3 install -r /etc/monitor-worker/requirements.txt
-python3 /etc/monitor-worker/scripts/heartbeats.py
+
+# Start heartbeats to ping backend every xxx seconds
+python3 /etc/monitor-worker/scripts/heartbeats.py &
+
+# Start python APIs
+cd /etc/monitor-worker/heartbeats/
+
+# DEV
+# uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+# PROD
+uvicorn main:app --host 0.0.0.0 --port 8000
