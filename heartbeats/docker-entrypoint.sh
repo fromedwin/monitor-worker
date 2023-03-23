f291cffc6bde878418926a6fb429409b31d52871
Sébastien Barbier <contact@sebastienbarbier.com>
2023-03-23 22:05:22 +0100

    Return timestamp to availability APIs

--- heartbeats/docker-entrypoint.sh
+++ heartbeats/docker-entrypoint.sh
@@ -8,6 +8,6 @@ python3 /etc/monitor-worker/scripts/heartbeats.py &
 cd /etc/monitor-worker/heartbeats/
 
 # DEV
-# uvicorn main:app --host 0.0.0.0 --port 8000 --reload
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 # PROD
-uvicorn main:app --host 0.0.0.0 --port 8000
+# uvicorn main:app --host 0.0.0.0 --port 8000
--- heartbeats/main.py
+++ heartbeats/main.py
@@ -1,13 +1,56 @@
-from typing import Union
+import requests
+import datetime
+import json
 
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"status": 200, "message": "OK"}
 
+# http://127.0.0.1:8001/fastapi/items/5?q=somequery
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/availability/{service_id}")
+def read_availability(service_id: int):
+
+    try:
+        href = 'http://prometheus:9090'
+
+        start = datetime.datetime.now().timestamp()
+
+        """
+            Fetch prometheus probe duration seconds data
+        """
+        response = requests.get(f'{href}/api/v1/query_range?query=probe_duration_seconds%7Bapplication="{service_id}"%7D&step=30&start={str(start-600)}&end={str(start)}')
+        response.raise_for_status()
+        content = json.loads(response.content)
+        for service in content['data']['result']:
+            service['metric']['title'] = ''
+
+        graph = json.dumps(content)
+        """
+            Fetch prometheus https expiration value
+        """
+        response = requests.get(f'{href}/api/v1/query?query=probe_ssl_earliest_cert_expiry%7Bapplication="{service_id}"%7D&time={str(start)}')
+        response.raise_for_status()
+        content = json.loads(response.content)
+        https = {}
+        for service in content['data']['result']:
+            https[service['metric']['service']] = int(service['value'][1]) # datetime.datetime.fromtimestamp()
+
+    except Exception as err:
+        content = {
+            'error': getattr(err, 'message', repr(err))
+        }
+
+    return {
+        "service_id": service_id, 
+        "https": https, 
+        "graph": graph,
+    }
